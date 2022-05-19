d09c3fea5ac730e96c204c576bc0e5d7c2ccbf3f
Babin Dangol <babindangol217@gmail.com>
2022-05-19 19:16:49 +0545

    commit 1

--- ecommerce_babin/wsgi.py
+++ ecommerce_babin/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for ecommerce_lab1 project.
+WSGI config for ecommerce_babin project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ecommerce_lab1.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ecommerce_babin.settings')
 
 application = get_wsgi_application()
