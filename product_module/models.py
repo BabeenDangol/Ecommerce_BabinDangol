728e6697cd5fe8b71c39334524919c517e1d90cc
Babin Dangol <babindangol217@gmail.com>
2022-06-02 07:03:24 +0545

    new hmlt

--- product_module/models.py
+++ product_module/models.py
@@ -5,11 +5,13 @@ from django.utils.html import mark_safe
 class Brand(models.Model):
     name = models.CharField(max_length=200)
     is_active = models.BooleanField()
+    
 class Category(models.Model):
     name = models.CharField(max_length=200)
     is_active = models.BooleanField()
     class Meta:
         verbose_name_plural = "Categories"
+        
 class Product(models.Model):
     name = models.CharField(max_length=200)
     price = models.FloatField()
