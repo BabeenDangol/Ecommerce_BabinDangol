4ca77e7b2ee4242b9168f18180c42b798ce68eeb
Babin Dangol <babindangol217@gmail.com>
2022-06-09 15:59:06 +0545

    changes 0906

--- product_module/views.py
+++ product_module/views.py
@@ -3,7 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 from django.db.models import Q
 from .models import Product, Brand, Category
-...
 def index(request):
     if request.method == "GET":
         category_id = request.GET.get("category")
@@ -25,20 +24,20 @@ def index(request):
             'search_query': '',
             }
         return render(request, 'index.html', context)
-    elif request.method == "POST": q = request.POST.get("query")
+    elif request.method == "POST": 
+        q = request.POST.get("query")
         if "-" in q:
             price_values = q.split("-")
             filter_query = Q(price__gte=price_values[0]) & Q(price__lte=price_values[1])
     else:
-        filter_query = Q(name__icontains=q) | Q(price__icontains=q) |
-Q(brand__name__icontains=q)
-products = Product.objects.filter(filter_query)
-categories = Category.objects.all()
-brands = Brand.objects.all()
-context = {
-'products': products,
-'categories': categories,
-'brands': brands,
-'search_query': q,
-}
-return render(request, 'index.html', context)
+        filter_query = Q(name__icontains=q) | Q(price__icontains=q) | Q(brand__name__icontains=q)
+        products = Product.objects.filter(filter_query)
+        categories = Category.objects.all()
+        brands = Brand.objects.all()
+        context = {
+        'products': products,
+        'categories': categories,
+        'brands': brands,
+        'search_query': q,
+    }
+    return render(request, 'index.html', context)
